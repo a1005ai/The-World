a2cad58f0dfd1f5280b5736dcb1dc0ee78ada026
a1019hk <a1019hk@aiit.ac.jp>
2011-08-27 17:13:25 +0900

    栗原追加

--- The World/src/TheWorld.java
+++ The World/src/TheWorld.java
@@ -3,5 +3,6 @@ public class TheWorld {
     public static void main(String[] args) {
         System.out.println("The world");
         System.out.println("Star Platinum");
+        System.out.println("Welcome to Hanoi");
     }
 }
