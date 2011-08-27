a6f06ce74a41798fcba9ded372d686a638eeb706
junya <junya.grad@gmail.com>
2011-08-27 17:13:12 +0900

    2行追加

--- The World/src/TheWorld.java
+++ The World/src/TheWorld.java
@@ -3,5 +3,7 @@ public class TheWorld {
     public static void main(String[] args) {
         System.out.println("The world");
         System.out.println("Star Platinum");
+        System.out.println("masunaga-desu");
+        System.out.println("junya-desu");
     }
 }
